c417d7ce225afc26f4846aae30b7a46606a92713
Prakhar Agnihotri <rounak316@gmail.com>
2021-12-09 12:50:33 +0530

    Gitleaks Parser: Fix for 8.1+ Version

--- dojo/tools/gitleaks/parser.py
+++ dojo/tools/gitleaks/parser.py
@@ -30,23 +30,24 @@ class GitleaksParser(object):
         dupes = dict()
         for issue in issues:
             line = None
-            file_path = issue["file"]
-            reason = issue["rule"]
+            file_path = issue["File"]
+            reason = issue["RuleID"]
             titleText = "Hard Coded " + reason
-            description = "**Commit:** " + issue["commitMessage"].rstrip("\n") + "\n"
-            description += "**Commit Hash:** " + issue["commit"] + "\n"
-            description += "**Commit Date:** " + issue["date"] + "\n"
-            description += "**Author:** " + issue["author"] + " <" + issue["email"] + ">" + "\n"
+            description = "**Commit:** " + issue["Message"].rstrip("\n") + "\n"
+            description += "**Commit Hash:** " + issue["Commit"] + "\n"
+            description += "**Commit Date:** " + issue["Date"] + "\n"
+            description += "**Author:** " + issue["Author"] + " <" + issue["Email"] + ">" + "\n"
             description += "**Reason:** " + reason + "\n"
             description += "**Path:** " + file_path + "\n"
-            if "lineNumber" in issue:
-                description += "**Line:** %i\n" % issue["lineNumber"]
-                line = issue["lineNumber"]
-            if "operation" in issue:
-                description += "**Operation:** " + issue["operation"] + "\n"
-            if "leakURL" in issue:
-                description += "**Leak URL:** [" + issue["leakURL"] + "](" + issue["leakURL"] + ")\n"
-            description += "\n**String Found:**\n\n```\n" + issue["line"].replace(issue["offender"], "REDACTED") + "\n```"
+            if "StartLine" in issue:
+                description += "**StartLine:** %i\n" % issue["StartLine"]
+                line = issue["StartLine"]
+            if "EndLine" in issue:
+                description += "**EndLine:** %i\n" % issue["EndLine"]
+            if "File" in issue:
+                description += "**File:** " + issue["File"] + "\n"
+
+            description += "\n**String Found:**\n\n```\n" + issue["StartLine"].replace(issue["Secret"], "REDACTED") + "\n```"
 
             severity = "High"
             if "Github" in reason or "AWS" in reason or "Heroku" in reason:
@@ -64,9 +65,9 @@ class GitleaksParser(object):
                 static_finding=True,
             )
             # manage tags
-            finding.unsaved_tags = issue.get("tags", "").split(', ')
+            finding.unsaved_tags = issue.get("Tags", [])#.split(', ')
 
-            dupe_key = hashlib.sha256((issue["offender"] + file_path + str(line)).encode("utf-8")).hexdigest()
+            dupe_key = hashlib.sha256((issue["Secret"] + file_path + str(line)).encode("utf-8")).hexdigest()
 
             if dupe_key not in dupes:
                 dupes[dupe_key] = finding
